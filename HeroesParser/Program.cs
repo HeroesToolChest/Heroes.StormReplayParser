143f8dfc04faa533ad924e0c2d585a8c389d9a3f
Kevin Oliva <kevin.k.oliva@gmail.com>
2020-08-07 16:53:48 -0500

    Removed HeroesDecode and added simple console

---
+++ HeroesParser/Program.cs
@@ -0,0 +1,29 @@
+﻿using Heroes.StormReplayParser;
+using System;
+using System.IO;
+
+namespace HeroesParser
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args != null && args.Length == 1 && File.Exists(args[0]))
+            {
+                StormReplayResult stormReplayResult = StormReplay.Parse(args[0], new ParseOptions()
+                {
+                    AllowPTR = true,
+                    ShouldParseGameEvents = true,
+                    ShouldParseMessageEvents = true,
+                    ShouldParseTrackerEvents = true,
+                });
+
+                Console.WriteLine(stormReplayResult.Status);
+            }
+            else
+            {
+                Console.WriteLine("No file.");
+            }
+        }
+    }
+}
