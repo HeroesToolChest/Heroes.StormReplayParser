0a868a6d16482f6546b663a7d44b7683a2e9b0c3
Kevin Oliva <kevin.k.oliva@gmail.com>
2022-04-17 20:57:45 -0500

    Added benchmark project

---
+++ Heroes.StormReplayParser.Benchmarks/StormReplayParserBenchmarks.cs
@@ -0,0 +1,21 @@
+﻿namespace Heroes.StormReplayParser.Benchmarks;
+
+[MemoryDiagnoser]
+public class StormReplayParserBenchmarks
+{
+    private readonly string _replaysFolder = "Replays";
+    private readonly string _replayFile = "SilverCity1_85267.StormR";
+
+    public StormReplayParserBenchmarks()
+    {
+    }
+
+    [Benchmark]
+    public void ParseReplay10()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            _ = StormReplay.Parse(Path.Join(_replaysFolder, _replayFile));
+        }
+    }
+}
