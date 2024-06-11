1c2391b48a55421bc14614ff844a48944bdf72ff
Kevin Oliva <kevin.k.oliva@gmail.com>
2024-06-11 13:51:34 -0500

    update to MSTest.sdk

--- Tests/Heroes.StormReplayParser.Tests/GlobalUsings.cs
+++ Tests/Heroes.StormReplayParser.Tests/GlobalUsings.cs
@@ -3,5 +3,4 @@ global using Heroes.StormReplayParser.MessageEvent;
 global using Heroes.StormReplayParser.Player;
 global using Heroes.StormReplayParser.Replay;
 global using Heroes.StormReplayParser.TrackerEvent;
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using System.Text.Json;
